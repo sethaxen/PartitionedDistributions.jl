de309872c52198b26d91183017c053ed631870a3
Seth Axen <seth@sethaxen.com>
2026-04-08 18:55:39 +0200

    feat: Support arbitrary indices and add conditional for MixtureModel (#6)
    * feat: Support linear and trailing indices
    
    * feat: Add linear indexing for ProductDistribution
    
    * fix: Correct error message
    
    * fix: Fix MatrixNormal accessors for Int indices
    
    * style: Run formatter
    
    * docs: Remove redundant description of indices
    
    * docs: Clarify constraints on keep_inds
    
    * fix: Only reshape if necessary
    
    * chore: Add StatsBase as direct dependency
    
    * fix: Correctly handle linear indices for product distributions
    
    * fix: Always pass an array to to_indices
    
    * fix: Fail earlier if product distribution would be empty
    
    * test: Update tests
    
    * feat: Add conditional for MixtureModel
    
    * test: Add missing tests
    
    * test: Randomly generate parameters
    
    * test: Fix test
    
    * test: Fix test
    
    * fix: Remove inaccessible method
    
    * test: Improve random pd mat generation
    
    * test: Directly test utils
    
    * test: Increase default tolerance
    
    * test: Refactor consistency tests

--- src/PartitionedDistributions.jl
+++ src/PartitionedDistributions.jl
@@ -4,7 +4,9 @@ using Distributions: Distributions
 using FillArrays: FillArrays
 using InvertedIndices: Not
 using LinearAlgebra: LinearAlgebra, I
+using LogExpFunctions: LogExpFunctions
 using PDMats: PDMats
+using StatsBase: StatsBase
 
 export conditional, marginal
 
